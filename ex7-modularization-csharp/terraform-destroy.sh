6a754f9cfad1f500f8912211979a0b618565f79e
Artur Rachwał <artur.rachwal@gmail.com>
2023-07-05 10:33:10 +0200

    commit (#2)

---
+++ ex7-modularization-csharp/terraform-destroy.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+
+terraform -chdir=./cdktf.out/stacks/azurerm \
+          validate
+terraform -chdir=./cdktf.out/stacks/azurerm \
+          destroy \
+          --auto-approve \
+          -var default_tenant_id=$TENANT_ID \
+          -var default_client_id=$TERRAFORM_CLIENT_ID \
+          -var default_client_secret=$TERRAFORM_CLIENT_SECRET \
+          -var default_subscription_id=$TERRAFORM_SUBSCRIPTION_ID
