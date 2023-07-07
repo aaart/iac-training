304dd92b2269ab4f6311c6e5372037f2766b3cc1
Artur Rachwał <artur.rachwal@softwareone.com>
2023-07-07 14:00:59 +0200

    commit

--- terraform/kv-to-repo-secrets/res/local.tf
+++ terraform/kv-to-repo-secrets/res/local.tf
@@ -1,3 +1,12 @@
 locals {
+  repository_name_no_owner = replace(var.github_repository_name, var.github_owner + "/", "")
+
   kv_secret_names = toset(split(" ", var.kv_secrets))
+
+  kv_secrets_map = {
+    for secret in local.kv_secret_names : secret => {
+      name = secret.replace(local.repository_name_no_owner + "---", "").replace("-", "_")
+      value = data.azurerm_key_vault_secret.secrets[secret].value
+    }
+  }
 }
