b72a76c509aef149b1b531a849b6760bcd2061e8
Artur Rachwał <artur.rachwal@softwareone.com>
2023-07-07 15:26:40 +0200

    commit

--- terraform/kv-to-repo-secrets/res/repository_secrets.tf
+++ terraform/kv-to-repo-secrets/res/repository_secrets.tf
@@ -1,7 +1,7 @@
 
 resource "github_actions_environment_secret" "secret" {
   for_each          = local.kv_secrets_map
-  repository        = var.github_repository_name
+  repository        = local.repository_name_no_owner
   environment       = var.github_repository_environment
   secret_name       = each.value.name
   plaintext_value   = each.value.value
